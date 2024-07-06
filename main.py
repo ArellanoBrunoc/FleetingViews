e962d715276b77630c58b8db19647b8e1e1c7c49
ArellanoBrunoc <arellanobruno@hotmail.com>
2024-07-06 03:48:18 -0500

    Tiding up code

--- main.py
+++ main.py
@@ -3,108 +3,82 @@ import FleetingViews as fv
 import time
 
 def main(page: ft.Page):
-
     # View definitions with specific configurations
     view_definitions = {
         'home': {
             'bgcolor': ft.colors.BLUE_GREY,
             'vertical_alignment': ft.MainAxisAlignment.CENTER,
-            'horizontal_alignment':ft.CrossAxisAlignment.CENTER
+            'horizontal_alignment': ft.CrossAxisAlignment.CENTER
         },
         'settings': {
             'bgcolor': ft.colors.AMBER_900,
             'vertical_alignment': ft.MainAxisAlignment.CENTER,
-            'horizontal_alignment':ft.CrossAxisAlignment.CENTER
+            'horizontal_alignment': ft.CrossAxisAlignment.CENTER
         },
         'projects': {
             'bgcolor': ft.colors.BLACK,
             'vertical_alignment': ft.MainAxisAlignment.CENTER,
-            'horizontal_alignment':ft.CrossAxisAlignment.CENTER
+            'horizontal_alignment': ft.CrossAxisAlignment.CENTER
         }
     }
 
     # Create instances of FleetingViews and configure views and controls
     fleetingViews = fv.create_views(view_definitions=view_definitions, page=page)
 
-    #Counter
+    # Counter function to update visit count
     def counter(view):
-        text = fleetingViews.views[view].controls[0].content.value
+        text = fleetingViews.views_dict[view].controls[0].content.value
         number_of_times = int(text.split(" ")[-2])
         number_of_times += 1
-        fleetingViews.views[view].controls[0].content.value = f"IM THE TEXT OF PAGE {view} {number_of_times} times"
+        fleetingViews.views_dict[view].controls[0].content.value = f"IM THE TEXT OF PAGE {view} {number_of_times} times"
         fleetingViews.view_go(view)
 
-
     # Set up content and buttons for the views
-
-
     fleetingViews.append("home", ft.Container(
-        width=page.width, 
-        height=page.height/2, 
-        content=ft.Text("IM THE TEXT OF PAGE HOME 0 times", size=40), 
-        alignment=ft.alignment.center))
-    
+        width=page.width,
+        height=page.height/2,
+        content=ft.Text("IM THE TEXT OF PAGE HOME 0 times", size=40),
+        alignment=ft.alignment.center
+    ))
+
     fleetingViews.append("projects", ft.Container(
-        width=page.width, 
-        height=page.height/2, 
-        content=ft.Text("IM THE TEXT OF PAGE PROJECTS 0 times", size=40), 
-        alignment=ft.alignment.center))
+        width=page.width,
+        height=page.height/2,
+        content=ft.Text("IM THE TEXT OF PAGE PROJECTS 0 times", size=40),
+        alignment=ft.alignment.center
+    ))
+
     fleetingViews.append("settings", ft.Container(
-        width=page.width, 
-        height=page.height/2, 
-        content=ft.Text("IM THE TEXT OF SETTINGS HOME 0 times", size=40), 
-        alignment=ft.alignment.center))
+        width=page.width,
+        height=page.height/2,
+        content=ft.Text("IM THE TEXT OF SETTINGS HOME 0 times", size=40),
+        alignment=ft.alignment.center
+    ))
 
-    #Buttons with allocations:
+    # Buttons with actions
+    to_projects = ft.ElevatedButton("Projects", on_click=lambda _: counter("projects"))
+    to_home = ft.ElevatedButton("Home", on_click=lambda _: counter("home"))
+    to_settings = ft.ElevatedButton("Settings", on_click=lambda _: counter("settings"))
+    back = ft.ElevatedButton("BACK!", on_click=lambda _: fleetingViews.go_back())
+    back_2 = ft.ElevatedButton("BACK!", on_click=lambda _: fleetingViews.go_back())
+    to_projects_2 = ft.ElevatedButton("Projects", on_click=lambda _: counter("projects"))
+    to_home_2 = ft.ElevatedButton("Home", on_click=lambda _: counter("home"))
+    to_settings_2 = ft.ElevatedButton("Settings", on_click=lambda _: counter("settings"))
+    back_3 = ft.ElevatedButton("BACK!", on_click=lambda _: fleetingViews.go_back())
 
-    to_projects = ft.ElevatedButton("Projects", 
-                                    on_click=lambda _: counter("projects"))
-    to_home = ft.ElevatedButton("Home", 
-                                on_click=lambda _: counter("home"))
-    to_settings = ft.ElevatedButton("Settings", 
-                                    on_click=lambda _: counter("settings"))
-    back = ft.ElevatedButton("BACK!", 
-                             on_click=lambda _: fleetingViews.go_back())
-    back_2 = ft.ElevatedButton("BACK!", 
-                               on_click=lambda _: fleetingViews.go_back())
-    to_projects_2 = ft.ElevatedButton("Projects", 
-                                      on_click=lambda _: counter("projects"))
-    to_home_2 = ft.ElevatedButton("Home", 
-                                  on_click=lambda _: counter("home"))
-    to_settings_2 = ft.ElevatedButton("Settings", 
-                                      on_click=lambda _: counter("settings"))
-    back_3 = ft.ElevatedButton("BACK!", 
-                               on_click=lambda _: fleetingViews.go_back())
+    # Append buttons to views
+    fleetingViews.append("home", [to_settings, to_projects, back])
+    fleetingViews.append("projects", [to_home, to_settings_2, back_2])
+    fleetingViews.append("settings", [to_home_2, to_projects_2, back_3])
 
-        # Set up content and buttons for the 'home' view
-    fleetingViews.append("home", 
-        [to_settings, to_projects, back]
-  
-    )
-
-    # Set up content and buttons for the 'projects' view
-    fleetingViews.append("projects", 
-        [to_home, to_settings_2, back_2]
-  
-    )
-
-    # Set up content and buttons for the 'settings' view
-    fleetingViews.append("settings", 
-        [to_home_2, to_projects_2, back_3]
-  
-    )
-
-    #The showed view is the last one to be declared in view_definitions
-
-    #This approach allows Views to easily communicate data as they can be seemlesly accesed using FleetingViews Object, i.e:
+    # Example to get data from a view
     def get_data(e, view):
-        data_in_project_view = fleetingViews.views['projects'].controls[0].content.value.split(" ")[-2] 
+        data_in_project_view = fleetingViews.views_dict['projects'].controls[0].content.value.split(" ")[-2]
         print(data_in_project_view)
         e.control.text = f"The {view} page has been visited {data_in_project_view} times"
         page.update()
 
-    get_data_button = ft.ElevatedButton(text="GET DATA", 
-                                    on_click=lambda e: get_data(e, "projects"))
+    get_data_button = ft.ElevatedButton(text="GET DATA", on_click=lambda e: get_data(e, "projects"))
     fleetingViews.append("home", get_data_button)
 
     page.update()
