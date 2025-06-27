ded0268e2fad99862ef1091259439755a0117d41
ArellanoBrunoc <arellanobruno@hotmail.com>
2025-06-27 10:43:02 -0500

    Updating setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="fleetingviews",
-    version="0.1.9.post4",
+    version="0.2",
     author="Bruno Arellano",
     author_email="arellanobruno@hotmail.com",
     py_modules=['FleetingViews'],
